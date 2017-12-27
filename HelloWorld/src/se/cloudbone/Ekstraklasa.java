80e6dd400f55e74996fb6a068e890d8503af81cf
jonas <jonas@192.168.88.128>
2017-12-27 18:05:45 +0100

    A new line was added.

--- HelloWorld/src/se/cloudbone/Ekstraklasa.java
+++ HelloWorld/src/se/cloudbone/Ekstraklasa.java
@@ -1,9 +1,12 @@
 package se.cloudbone;
 
+// En klass som är lite extra.
+
 public class Ekstraklasa {
 
 	public void cracovia() {
 		System.out.println("Cracovia");
+		System.out.println("En ny rad.");
 	}
 	
 }
